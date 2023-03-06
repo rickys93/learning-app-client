d803c0c1a86058f82adca2effb58628caa9705d2
Richard Swainson <rich_swainson@hotmail.co.uk>
2023-03-06 16:13:40 +0000

    Connect Categories page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,3 +1,4 @@
 export { default as NotFound } from "./NotFound";
+export { default as Categories } from "./Categories";
 export { default as LandingPage } from "./LandingPage";
 export { default as Account } from "./Account";
