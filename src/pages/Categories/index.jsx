623fc56ea6d3702450d6c7540759a312ab9efa57
Saeed Abdulle <104086456+Saabdulle@users.noreply.github.com>
2023-03-06 17:41:02 +0000

    function to fetch all categories

--- src/pages/Categories/index.jsx
+++ src/pages/Categories/index.jsx
@@ -5,6 +5,14 @@ export default function Categories() {
   const [stem, setStem] = useState([]);
   const [noneStem, setNoneStem] = useState([]);
 
+  useEffect(() => {
+    async function loadCategories() {
+      const response = await fetch("http://localhost:3000/categories");
+      const data = await response.json();
+      setCategory(data);
+    }
+    loadCategories();
+  }, []);
   return (
     <>
       <h1>Categories</h1>
