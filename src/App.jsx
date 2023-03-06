fb196997681c3420f8c84e8216c696fcad9976cd
dominika militowska <dominika.militowska@gmail.com>
2023-03-06 16:27:35 +0000

    flashcard

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import { NavBar } from "./components";
-import NotFound from "./pages/NotFound";
+import {NotFound, LandingPage, FlashcardPage} from "./pages";
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<NavBar />}>
+        {/* <Route path="/flashcards" element={<FlashcardPage />} /> */}
         <Route path="*" element={<NotFound />} />
       </Route>
     </Routes>
