8b8c2d79f9e0a3638f68361b2b0eca36227b4feb
dominika militowska <dominika.militowska@gmail.com>
2023-03-07 23:09:35 +0000

    navbar responsive

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink, Outlet } from "react-router-dom";
 
 import UserAccount from "../UserAccount";
@@ -6,16 +6,29 @@ import UserAccount from "../UserAccount";
 import './style.css'
 
 export default function NavBar() {
+
+  const [mobile, setMobile] = useState(true)
   return (
     <>
-      <header className="navbar">
-        <nav className="navlinks">
-          <NavLink to="/">Home</NavLink>
-          <NavLink to="/categories">Categories</NavLink>
-          <NavLink to="/flashcards">Memorize</NavLink>
-          <NavLink to="/quiz">Quiz</NavLink>
+      <header className="main">
+        <div className="navbar">
+        <div className="toggle-button" onClick={()=>setMobile(!mobile)}>
+          <span class="bar"></span>
+          <span class="bar"></span>
+          <span class="bar"></span>
+        </div>
+        <nav className="navlinks"  id={mobile? "hidden" : ""}>
+          <ul>
+          <li><NavLink to="/" className="a">Home</NavLink></li>
+          <li><NavLink to="/categories" className="a">Categories</NavLink></li>
+          <li><NavLink to="/flashcards" className="a">Memorize</NavLink></li>
+          <li><NavLink to="/quiz" className="a">Quiz</NavLink></li>
+          </ul>
         </nav>
+        </div>
+        <div className="login">
           <UserAccount/>
+          </div>
       </header>
       <Outlet />
     </>
