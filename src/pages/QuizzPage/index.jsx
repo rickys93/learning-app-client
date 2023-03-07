a6e5f066a4ca871dfc90c26c63ac2e55ad5c1551
Richard Swainson <rich_swainson@hotmail.co.uk>
2023-03-07 17:20:49 +0000

    Fix get /questions/categories/:id?limit=x endpoint

--- src/pages/QuizzPage/index.jsx
+++ src/pages/QuizzPage/index.jsx
@@ -3,58 +3,50 @@ import { QuizzCard } from '../../components'
 // import "./flashcard.css"
 
 export default function QuizzPage() {
-  const [categories, setCategories]= useState([])
-  const [amount, setAmount] = useState()
-  const [cat , setCat] = useState(1)
-const categoryEl = useRef()
-const amountEl = useRef()
-useEffect(()=>{
+    const [categories, setCategories]= useState([])
+    const [amount, setAmount] = useState()
+    const [cat , setCat] = useState(1)
+    const categoryEl = useRef()
+    const amountEl = useRef()
+    useEffect(()=>{
+        async function loadCategories() {
+            const response = await fetch(`http://localhost:3000/categories`);
+            const data = await response.json();
+            console.log(data)
+            setCategories(data);
+        };
 
- 
-  async function loadCategories() {
+        loadCategories();
 
-    const response = await fetch(`http://localhost:3000/categories`);
-    const data = await response.json();
-   console.log(data)
-    
-    setCategories(data);
+    },[])
 
-    
-};
-
-loadCategories();
-
-},[])
-
-function handleSubmit(e){
-  e.preventDefault()
-setAmount(amountEl.current.value)
-setCat(categoryEl.current.value)
-console.log(cat)
-
-}
+    function handleSubmit(e){
+        e.preventDefault()
+        setAmount(amountEl.current.value)
+        setCat(categoryEl.current.value)
+        console.log(cat)
+    }
 
   return (
     <>
-    <form className='header' onSubmit={handleSubmit}>
-      <div className='form-group'>
-        <label htmlFor = 'category'>Category</label>
-        <select id = "category" ref={categoryEl}>
-        {categories.map(category =>{
-          return <option value={category.id} key = {category.id}>{category.name}</option>
-        })}
-        </select>
-      </div>
-      <div className='form-group'>
-        <label htmlFor = 'amount'>Number of Questions</label>
-        <input type = "number" id = "amount" min = "1" step = "1" defaultValue = {10} ref={amountEl}></input>
-      </div>
-      <div className='form-group'>
-        <button className='generate_btn'>Generate</button>
-      </div>
-
-    </form>
-    <div ><QuizzCard id = {cat} limit = {amount}/></div>
+        <form className='header' onSubmit={handleSubmit}>
+            <div className='form-group'>
+                <label htmlFor = 'category'>Category</label>
+                <select id = "category" ref={categoryEl}>
+                {categories.map(category =>{
+                return <option value={category.id} key = {category.id}>{category.name}</option>
+                })}
+                </select>
+            </div>
+            <div className='form-group'>
+                <label htmlFor = 'amount'>Number of Questions</label>
+                <input type = "number" id = "amount" min = "1" step = "1" defaultValue = {10} ref={amountEl}></input>
+            </div>
+            <div className='form-group'>
+                <button className='generate_btn'>Generate</button>
+            </div>
+        </form>
+        <div ><QuizzCard id = {cat} limit = {amount}/></div>
     </>
   )
 }
