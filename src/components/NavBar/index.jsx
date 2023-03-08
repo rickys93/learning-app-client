aab5c33afbc23618d7b1c19770475ea291195017
Saeed Abdulle <104086456+Saabdulle@users.noreply.github.com>
2023-03-08 17:20:31 +0000

    navbar css completed

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -6,29 +6,34 @@ import UserAccount from "../UserAccount";
 import "./style.css";
 
 export default function NavBar() {
-
-  const [mobile, setMobile] = useState(false)
+  const [mobile, setMobile] = useState(false);
   return (
     <>
       <header className="main">
-      
         <div className="navbar">
-        <div className="toggle-button" onClick={()=>setMobile(!mobile)}>
-          <span className="bar"></span>
-          <span className="bar"></span>
-          <span className="bar"></span>
-        </div>
-        <nav className="navlinks"  id={mobile? "hidden" : ""}>
-          <ul>
-          <li><NavLink to="/" className="a">Home</NavLink></li>
-          <li><NavLink to="/categories" className="a">Categories</NavLink></li>
-        
-          </ul>
-        </nav>
+          <div className="toggle-button" onClick={() => setMobile(!mobile)}>
+            <span className="bar"></span>
+            <span className="bar"></span>
+            <span className="bar"></span>
+          </div>
+          <nav className="navlinks" id={mobile ? "hidden" : ""}>
+            <ul className="menu">
+              <li>
+                <NavLink to="/" className="a">
+                  Home
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/categories" className="a">
+                  Categories
+                </NavLink>
+              </li>
+            </ul>
+          </nav>
         </div>
         <div className="login">
-          <UserAccount/>
-          </div>
+          <UserAccount />
+        </div>
       </header>
       <Outlet />
     </>
--- src/components/UserAccount/index.jsx
+++ src/components/UserAccount/index.jsx
@@ -1,49 +1,55 @@
 import React, { useContext } from "react";
 import { NavLink, useNavigate } from "react-router-dom";
 import { UserContext, PopupContext } from "../../App";
-import { Login, Signup } from '../index'
+import { Login, Signup } from "../index";
+import { Link } from "react-router-dom";
 
-import './style.css'
+import "./style.css";
 
 function UserAccount() {
-    const navigate = useNavigate()
-    const {user, setUser} = useContext(UserContext)
-    const { openPopup } = useContext(PopupContext)
+  const navigate = useNavigate();
+  const { user, setUser } = useContext(UserContext);
+  const { openPopup } = useContext(PopupContext);
 
-    const handleLoginClick = () => {
-        openPopup(<Login/>)
-    }
+  const handleLoginClick = () => {
+    openPopup(<Login />);
+  };
 
-    const handleSignupClick = () => {
-        openPopup(<Signup/>)
-    }
-    
-    const handleLogOut = () => {
-        console.log("here")
-        localStorage.clear();
-        navigate("/")
-        window.location.reload()
-    }
-    return (
-        <div className="userAccount">
-            {Object.keys(user).length ?
-                <>
-                    <h2>Welcome, {user.first_name}!</h2>
-                    <button
-                        onClick={handleLogOut} className="logout"
-                    >
-                        Log Out
-                    </button>
-                </>
-                :
-                <>
-                    <button onClick={handleLoginClick}>Log In</button>
-                    <button onClick={handleSignupClick}>Sign Up</button>
-                </>
-            }
-        </div>
+  const handleSignupClick = () => {
+    openPopup(<Signup />);
+  };
 
-    )
+  const handleLogOut = () => {
+    console.log("here");
+    localStorage.clear();
+    navigate("/");
+    window.location.reload();
+  };
+  return (
+    <div className="userAccount">
+      {Object.keys(user).length ? (
+        <>
+          <h2>Welcome, {user.first_name}!</h2>
+          <button onClick={handleLogOut} className="logout">
+            Log Out
+          </button>
+        </>
+      ) : (
+        <>
+          <ul className="menu">
+            <li>
+              <Link onClick={handleLoginClick}>Log In</Link>
+            </li>
+            <li>
+              <Link onClick={handleSignupClick}>Sign Up</Link>
+            </li>
+          </ul>
+          {/* <button onClick={handleLoginClick}>Log In</button>
+          <button onClick={handleSignupClick}>Sign Up</button> */}
+        </>
+      )}
+    </div>
+  );
 }
 
-export default UserAccount
+export default UserAccount;
