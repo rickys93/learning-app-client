0b25b0627877d67bbf98b648e9c04b6205e811d9
Richard Swainson <rich_swainson@hotmail.co.uk>
2023-03-08 11:53:52 +0000

    Type QuizzPage -> QuizPage

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,5 +4,5 @@ export { default as LandingPage } from "./LandingPage";
 export { default as Login } from "./Login";
 export { default as Signup } from "./Signup";
 export { default as FlashcardPage } from "./FlashcardPage";
-export { default as QuizzPage } from "./QuizzPage";
+export { default as QuizPage } from "./QuizPage";
 export { default as NewFlashcard } from "./NewFlashcard";
