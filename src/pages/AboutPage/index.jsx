395bdb7f986f9b9fa5d822c3c088aec9e514a927
Saeed Abdulle <104086456+Saabdulle@users.noreply.github.com>
2023-03-09 17:04:22 +0000

    about us page styled

--- src/pages/AboutPage/index.jsx
+++ src/pages/AboutPage/index.jsx
@@ -1,34 +1,69 @@
 import React, { useState } from "react";
 
+import "./about.css";
+
 export default function AboutUsPage() {
   const [feedback, setFeedback] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // Code to submit feedback goes here
-    setFeedback("");
+    if (feedback.trim() !== "") {
+      alert("Thank you for your feedback!");
+      window.location.reload();
+    } else {
+      alert("Please write feedback before submitting!");
+    }
   };
 
-  const handleChange = (e) => {
+  const handleFeedbackChange = (e) => {
     setFeedback(e.target.value);
   };
 
+  const handleChange = (e) => {
+    const feedback = e.target.value;
+    if (feedback === "") {
+      alert("Please write a feedback before submitting!");
+    } else {
+      setFeedback(feedback);
+      alert("Thank you for your feedback.");
+      window.location.reload();
+    }
+  };
+
   return (
-    <div>
+    <>
       <h2>About Us</h2>
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cupiditate
-        aut, molestias minus aliquam eius, magnam voluptatibus dolor mollitia
-        vitae eos unde, corrupti perspiciatis delectus pariatur impedit minima
-        quia eum deleniti.
-      </p>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Feedback:
-          <textarea value={feedback} onChange={handleChange} />
-        </label>
-        <button type="submit">Submit Feedback</button>
-      </form>
-    </div>
+      <div className="top-div">
+        <div className="mid-div">
+          <p>
+            Crammer Education has developed this learning app because of an
+            increase in demand for remote learning since the pandemic began.
+            Upsurge interest in quiz and flashcard by students became more
+            prominent and to meet this demand, Crammer UK has made this apps
+            with a new product that will support students across the country
+            with their revision.
+          </p>
+
+          <form onSubmit={handleSubmit}>
+            <label>
+              Feedback:&nbsp;
+              <textarea
+                className="textarea-1"
+                value={feedback}
+                onChange={handleFeedbackChange}
+              />
+            </label>
+            <br />
+            <button className="ta-btn" type="submit">
+              Submit
+            </button>
+          </form>
+          <h4 className="ab-title">
+            Contact us: 020 802 0802&nbsp;&nbsp; |
+            &nbsp;&nbsp;crammeruk@crammeruk.co.uk
+          </h4>
+        </div>
+      </div>
+    </>
   );
 }
